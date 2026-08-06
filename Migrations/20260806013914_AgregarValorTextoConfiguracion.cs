a7a95407f55202255a15c9dc91cb5c3753d3e00a
juan david <juandavidmoscoso123@gmail.com>
2026-08-05 20:43:06 -0500

    Agregar migracion de valorTexto

---
+++ Migrations/20260806013914_AgregarValorTextoConfiguracion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parquing.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarValorTextoConfiguracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HoraIngreso",
+                table: "Vehiculos",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValorFecha",
+                table: "Configuraciones",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ValorTexto",
+                table: "Configuraciones",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValorTexto",
+                table: "Configuraciones");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "HoraIngreso",
+                table: "Vehiculos",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ValorFecha",
+                table: "Configuraciones",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+        }
+    }
+}
